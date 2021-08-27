71e975298accf6fe63824fdca6bc1a7a22dfca2e
Andrew Koster <andrewkoster95@gmail.com>
2021-08-27 07:09:14 -0400

    ready to deploy

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import { useQuery } from '@apollo/client';
 
 import Cart from '../components/Cart';
